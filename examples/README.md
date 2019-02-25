0021d94298fbe74b695b0ba8acb4443d324c2f01
Ryan Hunter <rymalhunt@gmail.com>
2019-02-24 18:13:09 -0700

    Get basic template working with updated README

---
+++ examples/README.md
@@ -0,0 +1,15 @@
+# Rails Application
+
+This is a newly generated Rails application using
+[RH Productions' Jumpstart](https://github.com/ryan-hunter-pc/jumpstart)
+
+**You'll want to edit this README to document the specific application this will become.**
+
+# Development Environment
+
+To setup a new development environment:
+
+1. Clone the codebase
+2. Run the setup script: `bin/setup`
+3. Run the development environment: `foreman start -f Procfile.dev`
+3. (Optional) Run a TDD environment in a separate terminal: `bundle exec guard`
---
+++ template.rb
@@ -0,0 +1,85 @@
+require "fileutils"
+require "shellwords"
+
+# Copied from: https://github.com/excid3/jumpstart
+# Add this template directory to source_paths so that Thor actions like
+# copy_file and template resolve against our source files. If this file was
+# invoked remotely via HTTP, that means the files are not present locally.
+# In that case, use `git clone` to download them to a local temporary dir.
+def add_template_repository_to_source_path
+  if __FILE__ =~ %r{\Ahttps?://}
+    require "tmpdir"
+    source_paths.unshift(tempdir = Dir.mktmpdir("jumpstart-"))
+    at_exit { FileUtils.remove_entry(tempdir) }
+    git clone: [
+      "--quiet",
+      "https://github.com/ryan-hunter-pc/jumpstart.git",
+      tempdir
+    ].map(&:shellescape).join(" ")
+
+    if (branch = __FILE__[%r{jumpstart/(.+)/template.rb}, 1])
+      Dir.chdir(tempdir) { git checkout: branch }
+    end
+  else
+    source_paths.unshift(File.dirname(__FILE__))
+  end
+end
+
+def add_gems
+  gem 'colorize'
+end
+
+def initialize_git_repository
+  git :init
+  git add: "."
+  git commit: %Q{ -m 'Generate and bundle rails app using ryan-hunter-pc/jumpstart' }
+end
+
+def update_setup_script
+  gsub_file 'bin/setup', "# system('bin/yarn')", "system('yarn')"
+  gsub_file 'bin/setup', "system! 'bin/rails db:setup'", "system! 'bundle exec rails db:setup'"
+  comment_lines 'bin/setup', /Restarting application server/
+  comment_lines 'bin/setup', /rails restart/
+  git add: '.'
+  git commit: %Q{ -m "Update setup script" }
+end
+
+def setup_heroku_apps
+  return unless yes?('Would you like to create Heroku staging and production applications now?')
+  heroku_app_name = ask('What should we name the Heroku apps? e.g. my-app')
+
+  # TODO: create the Heroku apps
+
+  # Add heroku remotes to the setup script
+  heroku_setup_block = <<-RUBY
+    if system 'heroku'
+      puts "\n== Setting Heroku remotes =="
+      system "heroku git:remote -a #{heroku_app_name}-staging -r staging"
+      system "heroku git:remote -a #{heroku_app_name}-production -r production"
+    end
+  RUBY
+
+  insert_into_file "bin/setup", "\n#{heroku_setup_block}", after: "system!('bundle install')\n"
+end
+
+def copy_example_readme
+  remove_file 'README.md'
+  copy_file 'examples/README.md', 'README.md'
+  git add: '.'
+  git commit: %Q{ -m "Update README" }
+end
+
+
+#==========================================================================
+# Main Setup Script
+#==========================================================================
+
+add_template_repository_to_source_path
+add_gems
+
+after_bundle do
+  initialize_git_repository
+  update_setup_script
+  # setup_heroku_apps # FIXME: need to finish this method before uncommenting
+  copy_example_readme
+end
