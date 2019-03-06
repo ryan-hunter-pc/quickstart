801880048ce56362d5d29acd65af3b87bc2fc2fb
Ryan Hunter <rymalhunt@gmail.com>
2019-03-06 15:43:19 -0700

    Add an idempotent script to create/refresh a test application in a ./testapp/ directory, for quicker development loop for this template

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# remove previously generated test app
+if [ -d "./testapp" ]; then
+  cd testapp
+  spring stop
+  bundle exec rails db:drop
+  cd ..
+  rm -rf testapp
+fi
+
+# generate test app with local copy of jumpstart
+rails new testapp -T -d=postgresql --webpack -m ./template.rb
+
+# setup and run development server (optional - comment out to skip)
+cd testapp
+bin/setup
+# bundle exec rspec -f d
+bundle exec rails db:migrate
+foreman start -f Procfile.dev
+
+
+# TODO
+
+# High Voltage
+# Money-Rails
